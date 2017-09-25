ff87dadd717c308f3d863e781aa7aebcc629588d
Breno Nogueira <brenozxc@hotmail.com>
2017-09-25 19:52:52 -0400

    class

---
+++ PB1CSTADS2207/Models/Automovel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace PB1CSTADS2207.Models
+{
+    public class Automovel
+    {
+        int AutomovelId;
+        string Descricao;
+        string Marca;
+        bool Disponivel;
+        DateTime DataCadastro;
+
+    }
+}
